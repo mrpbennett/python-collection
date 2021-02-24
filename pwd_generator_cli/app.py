f1137018a2dc24a40cb8bbf8e43faa18a59d7c11
Paul Bennett <pbennett.uk@gmail.com>
2021-02-24 19:34:04 +0000

    changing it to generate a token
    depending on users size requirements

--- pwd_generator_cli/app.py
+++ pwd_generator_cli/app.py
@@ -4,31 +4,30 @@ from PyInquirer import prompt
 
 questions = [
     {
-        'type': 'list',
-        'name': 'tasks',
-        'message': 'What would you like to generate?',
-        'choices': [
-            'Password',
-            'Hexadecimal',
-
-        ]
+        "type": "list",
+        "name": "tasks",
+        "message": "What would you like to generate?",
+        "choices": ["Password", "Token"],
     }
 ]
 
 answers = prompt(questions)
 
-# password generator that uses all case type, digits and special charactors 
-def password_generator(size=25, chars=string.ascii_letters + string.digits + string.punctuation):
-    return ''.join(secrets.SystemRandom().choice(chars) for _ in range(size))
-    
+# password generator that uses all case type, digits and special charactors
+def password_generator(
+    size=25, chars=string.ascii_letters + string.digits + string.punctuation
+):
+    return "".join(secrets.SystemRandom().choice(chars) for _ in range(size))
 
-# hexcode generator that takes a number as an argument and generates a token of that length
-def hex_generator(size):
+
+# token generator that takes a number as an argument and generates a token of that length
+def token_generator(size):
     return secrets.token_hex(size)
 
 
-if answers['tasks'] == 'Password':
+if answers["tasks"] == "Password":
     print(password_generator(25))
 
-if answers['tasks'] == 'Hexadecimal':
-    print(hex_generator(25))
+if answers["tasks"] == "Token":
+    size = input("how many bytes?: ")
+    print(token_generator(int(size)))
